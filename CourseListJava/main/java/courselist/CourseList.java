81c545d34eb707ecba0fe2f74db151a69df1db12
Wesley Kambale <spartanwk@gmail.com>
2021-03-06 16:28:21 +0300

    in Java

---
+++ CourseListJava/main/java/courselist/CourseList.java
@@ -0,0 +1,12 @@
+package com.example.courselist;
+
+import java.util.Arrays;
+
+public class CourseList {
+
+        public static void mai(String[] args){
+            String courseList[] = {"Artificial Intelligence, ", "Entrepreneurship, ", "Computer and Network Security, ", "Mobile Programming, ", "Data Communications and Networks, ", "Automata, Complexity and Computability."};
+            System.out.println(Arrays.toString(courseList));
+        }
+    }
+    
